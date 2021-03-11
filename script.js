8c595f78a893391c72e461efb9adbeee921055f8
DariaVarlamova90 <da.varlamova@yandex.ru>
2021-03-11 17:06:28 +0300

    перенос текста из input в div (не работает)

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelector('.input__btn').addEventListener('click', function(){
-    let taskDiv = document.createEvent('div');
+    let taskDiv = document.createElement('div');
+    let taskText = document.querySelector('.input__text').value;
     taskDiv.className = 'task__wrapper';
-    taskDiv.innerHTML = 'DIV task__wrapper';
+    taskDiv.innerHTML = `<div class = 'task__wrapper'>${taskText.value}</div>`;
     document.querySelector('.tasks__wrapper').appendChild(taskDiv);
 })
